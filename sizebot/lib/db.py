f60d5a12ea6efd848ae309b39a6b9c111302aef2
Natalie Fearnley <nfearnley@gmail.com>
2021-03-13 17:14:16 -0500

    Added SVField, WVField, TVField, and SnowflakeField

--- sizebot/lib/db.py
+++ sizebot/lib/db.py
@@ -1,5 +1,7 @@
 import arrow
-from mongoengine.fields import DateTimeField
+from mongoengine.fields import DateTimeField, StringField
+
+from sizebot.lib.units import SV, WV, TV
 
 
 class ArrowField(DateTimeField):
@@ -13,3 +15,62 @@ class ArrowField(DateTimeField):
         if value is not None:
             value = arrow.get(value)
         return value
+
+
+class UnitField(StringField):
+    UNIT_CLASS = None
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        if isinstance(value, str):
+            value = self.UNIT_CLASS(value)
+        return value
+
+    def to_mongo(self, value):
+        return str(value)
+
+    def validate(self, value):
+        if not isinstance(value, self.UNIT_CLASS):
+            self.error(f"{value} is not a {self.UNIT_CLASS.__name__}")
+
+
+class SVField(UnitField):
+    UNIT_CLASS = SV
+
+
+class WVField(UnitField):
+    UNIT_CLASS = WV
+
+
+class TVField(UnitField):
+    UNIT_CLASS = TV
+
+
+class SnowflakeField(StringField):
+    def to_mongo(self, value):
+        print("TO_MONGO")
+        if value is not None:
+            value = str(value)
+        return value
+
+    def to_python(self, value):
+        print("TO_PYTHON")
+        value = super().to_python(value)
+        if value is not None:
+            value = int(value)
+        return value
+
+    def validate(self, value):
+        print("VALIDATE")
+        min_value = 0x0000000000000000
+        max_value = 0xFFFFFFFFFFFFFFFF
+        try:
+            value = int(value)
+        except (TypeError, ValueError):
+            self.error(f"{value} could not be converted to int")
+
+        if value < min_value:
+            self.error("Snowflake value is too small")
+
+        if value > max_value:
+            self.error("Snowflake value is too large")
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-import pytest
+from sizebot.lib.units import SV, WV, TV
 
 import arrow
-from mongoengine import connect, disconnect, Document
+import pytest
+from mongoengine import Document, connect, disconnect
+from mongoengine.errors import ValidationError
 
-from sizebot.lib.db import ArrowField
+from sizebot.lib.db import ArrowField, SnowflakeField, SVField, WVField, TVField
 
 
 @pytest.fixture
@@ -14,40 +16,37 @@ def db():
     disconnect()
 
 
-class ArrowTest(Document):
+class ArrowDoc(Document):
     mydate = ArrowField()
-
-    def __str__(self):
-        return repr(self.mydate)
 
 
 def test_arrow_none(db):
-    t = ArrowTest(mydate=None)
+    t = ArrowDoc(mydate=None)
     t.save()
-    t_saved = ArrowTest.objects(id=t.id).first()
+    t_saved = ArrowDoc.objects(id=t.id).first()
     assert t_saved.mydate is None
 
 
 def test_arrow_int(db):
-    t = ArrowTest(mydate=0)
+    t = ArrowDoc(mydate=0)
     t.save()
-    t_saved = ArrowTest.objects(id=t.id).first()
+    t_saved = ArrowDoc.objects(id=t.id).first()
     assert isinstance(t_saved.mydate, arrow.Arrow)
     assert t_saved.mydate == arrow.get("1970-01-01T00:00:00+00:00")
 
 
 def test_arrow_datetime(db):
-    t = ArrowTest(mydate=datetime.utcfromtimestamp(0))
+    t = ArrowDoc(mydate=datetime.utcfromtimestamp(0))
     t.save()
-    t_saved = ArrowTest.objects(id=t.id).first()
+    t_saved = ArrowDoc.objects(id=t.id).first()
     assert isinstance(t_saved.mydate, arrow.Arrow)
     assert t_saved.mydate == arrow.get("1970-01-01T00:00:00+00:00")
 
 
 def test_arrow_arrow(db):
-    t = ArrowTest(mydate=arrow.Arrow.utcfromtimestamp(0))
+    t = ArrowDoc(mydate=arrow.Arrow.utcfromtimestamp(0))
     t.save()
-    t_saved = ArrowTest.objects(id=t.id).first()
+    t_saved = ArrowDoc.objects(id=t.id).first()
     assert isinstance(t_saved.mydate, arrow.Arrow)
     assert t_saved.mydate == arrow.get("1970-01-01T00:00:00+00:00")
 
@@ -55,6 +54,109 @@ def test_arrow_arrow(db):
 def test_compare(db):
     a_small = arrow.Arrow.utcfromtimestamp(0)
     a_big = arrow.Arrow.utcfromtimestamp(1000)
-    t_small = ArrowTest(mydate=a_small)
+    t_small = ArrowDoc(mydate=a_small)
     t_small.save()
-    assert ArrowTest.objects(mydate__lte=a_big).count() == 1
+    assert ArrowDoc.objects(mydate__lte=a_big).count() == 1
+
+
+class SnowflakeDoc(Document):
+    snow = SnowflakeField()
+
+
+def test_snowflake_max(db):
+    max_snowflake = 0xFFFFFFFFFFFFFFFF
+    doc = SnowflakeDoc(snow=max_snowflake)
+    doc.save()
+    doc_saved = SnowflakeDoc.objects(id=doc.id).first()
+    assert doc_saved.snow == max_snowflake
+
+
+def test_snowflake_min(db):
+    min_snowflake = 0x0000000000000000
+    doc = SnowflakeDoc(snow=min_snowflake)
+    doc.save()
+    doc_saved = SnowflakeDoc.objects(id=doc.id).first()
+    assert doc_saved.snow == min_snowflake
+
+
+def test_snowflake_none(db):
+    doc = SnowflakeDoc(snow=None)
+    doc.save()
+    doc_saved = SnowflakeDoc.objects(id=doc.id).first()
+    assert doc_saved.snow is None
+
+
+def test_snowflake_invalid(db):
+    invalid_snowflake = -1
+    doc = SnowflakeDoc(snow=invalid_snowflake)
+    with pytest.raises(ValidationError):
+        doc.save()
+
+
+class UnitsDoc(Document):
+    sv_val = SVField()
+    wv_val = WVField()
+    tv_val = TVField()
+
+
+def test_sv(db):
+    doc = UnitsDoc(sv_val=SV(1000))
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert isinstance(doc_saved.sv_val, SV)
+    assert doc_saved.sv_val == SV(1000)
+
+
+def test_sv_none(db):
+    doc = UnitsDoc(sv_val=None)
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert doc_saved.sv_val is None
+
+
+def test_sv_invalid(db):
+    doc = UnitsDoc(sv_val=WV(1000))
+    with pytest.raises(ValidationError):
+        doc.save()
+
+
+def test_wv(db):
+    doc = UnitsDoc(wv_val=WV(1000))
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert isinstance(doc_saved.wv_val, WV)
+    assert doc_saved.wv_val == WV(1000)
+
+
+def test_wv_none(db):
+    doc = UnitsDoc(wv_val=None)
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert doc_saved.wv_val is None
+
+
+def test_wv_invalid(db):
+    doc = UnitsDoc(wv_val=TV(1000))
+    with pytest.raises(ValidationError):
+        doc.save()
+
+
+def test_tv(db):
+    doc = UnitsDoc(tv_val=TV(1000))
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert isinstance(doc_saved.tv_val, TV)
+    assert doc_saved.tv_val == TV(1000)
+
+
+def test_tv_none(db):
+    doc = UnitsDoc(tv_val=None)
+    doc.save()
+    doc_saved = UnitsDoc.objects(id=doc.id).first()
+    assert doc_saved.tv_val is None
+
+
+def test_tv_invalid(db):
+    doc = UnitsDoc(tv_val=SV(1000))
+    with pytest.raises(ValidationError):
+        doc.save()
